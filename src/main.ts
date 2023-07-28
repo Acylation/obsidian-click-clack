c2e75de4cd2eda48b2a394eba49bd25aa747d9b3
Acylation <532117255@qq.com>
2023-07-29 03:35:33 +0800

    Manual initial commit

---
+++ src/main.ts
@@ -0,0 +1,103 @@
+import {
+	App,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from 'obsidian';
+import * as path from 'path';
+
+const AUDIO_ID = 'typewriter-sound-plugin-audio';
+
+interface TWSoundSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: TWSoundSettings = {
+	mySetting: 'default',
+};
+
+export default class TWSoundPlugin extends Plugin {
+	settings: TWSoundSettings;
+
+	async onload() {
+		await this.loadSettings();
+
+		const audioEl = document.createElement('audio');
+		audioEl.id = AUDIO_ID;
+		document.body.appendChild(audioEl);
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new SampleSettingTab(this.app, this));
+
+		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// Using this function will automatically remove the event listener when this plugin is disabled.
+		this.registerDomEvent(document, 'keydown', (evt: KeyboardEvent) => {
+			console.log('keypress', evt);
+			audioEl.src = buildPluginStaticResourceSrc(
+				this,
+				'resource/selectric-mode/selectric-type.wav'
+			);
+			console.log(audioEl);
+			console.log(audioEl.src);
+			audioEl.play();
+		});
+	}
+
+	onunload() {
+		document.getElementById(AUDIO_ID)?.remove();
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+// https://forum.obsidian.md/t/support-for-assets-in-plugins/25837/2
+export function buildPluginStaticResourceSrc(plug: Plugin, assetPath: string) {
+	return plug.app.vault.adapter.getResourcePath(
+		path.join(
+			plug.app.vault.configDir,
+			'plugins',
+			plug.manifest.id,
+			assetPath
+		)
+	);
+}
+
+class SampleSettingTab extends PluginSettingTab {
+	plugin: TWSoundPlugin;
+
+	constructor(app: App, plugin: TWSoundPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder('Enter your secret')
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
+	}
+}
---
+++ todo.md
@@ -0,0 +1,8 @@
+# Typewriter Sound Plugin Todo List
+
+source: <https://forum.obsidian.md/t/typewriter-sounds/15474>
+先行者：<https://github.com/adityaketkar/obsidian-typewriter-sounds>
+
+其他编辑器拓展: <https://github.com/rbanffy/selectric-mode/blob/master/selectric-mode.el>
+
+- fill package.json
